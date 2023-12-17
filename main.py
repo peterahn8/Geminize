ee7592da0d642ee81b7e39fa7da07837cd527626
Peter Ahn <peterahn8@gmail.com>
2023-12-16 19:45:56 -0500

    Set up basic text+image input

--- main.py
+++ main.py
@@ -2,11 +2,19 @@ import pathlib
 import textwrap
 
 from decouple import config
+from PIL import Image
 import google.generativeai as genai
 
 genai.configure(api_key=config('API_KEY'))
 
-# model = genai.GenerativeModel('gemini-pro')
+model = genai.GenerativeModel('gemini-pro-vision')
 
 # response = model.generate_content("What is the meaning of life?")
 # print(response.text)
+
+image_path = './images/test.png' 
+img = Image.open(image_path)
+
+response = model.generate_content(["What is the first value in this image. Only respond with the value itself.", img], stream=True)
+response.resolve()
+print(response.text)
