b1f330dd86e32cc61f5488786f7b3c7f0f4536f0
Thanoj Singh <thanoj.singh@gmail.com>
2023-12-17 12:48:45 -0500

    init

--- frontend/main.js
+++ frontend/main.js
@@ -1,1 +1,23 @@
-stuff
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+ctx.fillStyle = "green";
+ctx.fillRect(10, 10, 150, 100);
+
+function startSocket() {
+    // Create WebSocket connection.
+    const socket = new WebSocket("ws://localhost:8080");
+
+    // Connection opened
+    socket.addEventListener("open", (event) => {
+        socket.send("Hello Server!");
+    });
+
+    // Listen for messages
+    socket.addEventListener("message", (event) => {
+        console.log("Message from server ", event.data);
+    });
+}
+
+// init
+startSocket();
