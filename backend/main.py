3c6fe2b3c348087486fc503d2e7c1b6d83dec51e
Thanoj Singh <thanoj.singh@gmail.com>
2023-12-17 21:51:58 -0500

    clean up imports

--- backend/main.py
+++ backend/main.py
@@ -2,9 +2,7 @@ import io
 
 from PIL import Image
 from decouple import config
-from flask import Flask
-from flask import send_from_directory
-from flask import request
+from flask import Flask, send_from_directory, request
 from flask_socketio import SocketIO, send
 
 import google.generativeai as genai
@@ -12,7 +10,6 @@ import google.generativeai as genai
 genai.configure(api_key=config('API_KEY'))
 model = genai.GenerativeModel('gemini-pro-vision')
 
-# Web Stuff
 app = Flask(__name__)
 socketio = SocketIO(app)
 
@@ -22,6 +19,11 @@ def main_route(path):
     if path == '':
         path = 'index.html'
     return send_from_directory("../frontend", path)
+
+@socketio.on("join")
+def handle_join(data):
+    sid = request.sid
+    join_room(data)
 
 @socketio.on("drawing")
 def handle_drawing(data):
@@ -40,9 +42,3 @@ def handle_drawing(data):
     print(response.text)
 
 socketio.run(app, host="0.0.0.0", port=3000)
-# Don't need this anymore?
-# app.run(host="0.0.0.0", port=3000)
-
-## try this if you want to see all the properties of the 'response' object above
-# for key, value in vars(response).items():
-#     print(f"{key}: {value}")
